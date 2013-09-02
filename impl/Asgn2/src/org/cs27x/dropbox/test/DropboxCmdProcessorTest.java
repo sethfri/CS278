e0d4b0774904568146df94a84e03694f54bcbbc9
Seth Friedman <seth.n.friedman@vanderbilt.edu>
2013-09-01 23:33:18 -0500

    Add test stubs for DropboxCmdProcessor

---
+++ impl/Asgn2/src/org/cs27x/dropbox/test/DropboxCmdProcessorTest.java
@@ -0,0 +1,54 @@
+package org.cs27x.dropbox.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DropboxCmdProcessorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDropboxCmdProcessor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateFileState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCmdReceived() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConnected() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisconnected() {
+		fail("Not yet implemented");
+	}
+
+}
