bdda8d512ff8796474cc0e4d22e3ad1893e19149
Seth Friedman <sethfri@gmail.com>
2013-11-21 06:32:54 -0600

    Add test coordinates

--- impl/Final/Final Project/Final Project/FPMapViewController.m
+++ impl/Final/Final Project/Final Project/FPMapViewController.m
@@ -23,6 +23,31 @@
     [super viewDidLoad];
     
     self.mapHasBeenCenteredAroundUser = NO;
+    
+    MKPointAnnotation *firstAnnotation = [[MKPointAnnotation alloc] init];
+    firstAnnotation.coordinate = CLLocationCoordinate2DMake(36.139483, -86.8331);
+    MKPinAnnotationView *firstPin = [[MKPinAnnotationView alloc] initWithAnnotation:firstAnnotation
+                                                                    reuseIdentifier:@"PinAnnotation"];
+    
+    MKPointAnnotation *secondAnnotation = [[MKPointAnnotation alloc] init];
+    secondAnnotation.coordinate = CLLocationCoordinate2DMake(36.157233, -86.795583);
+    MKPinAnnotationView *secondPin = [[MKPinAnnotationView alloc] initWithAnnotation:secondAnnotation
+                                                                    reuseIdentifier:@"PinAnnotation"];
+    
+    MKPointAnnotation *thirdAnnotation = [[MKPointAnnotation alloc] init];
+    thirdAnnotation.coordinate = CLLocationCoordinate2DMake(36.133533, -86.760833);
+    MKPinAnnotationView *thirdPin = [[MKPinAnnotationView alloc] initWithAnnotation:thirdAnnotation
+                                                                    reuseIdentifier:@"PinAnnotation"];
+    
+    MKPointAnnotation *fourthAnnotation = [[MKPointAnnotation alloc] init];
+    fourthAnnotation.coordinate = CLLocationCoordinate2DMake(36.11155, -86.7973);
+    MKPinAnnotationView *fourthPin = [[MKPinAnnotationView alloc] initWithAnnotation:fourthAnnotation
+                                                                    reuseIdentifier:@"PinAnnotation"];
+    
+    [self.mapView addAnnotations:@[firstPin,
+                                   secondPin,
+                                   thirdPin,
+                                   fourthPin]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,7 +61,7 @@
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
     if (!self.mapHasBeenCenteredAroundUser) {
         CLLocationCoordinate2D userLocationCoordinate = userLocation.location.coordinate;
-        MKCoordinateSpan regionSpan = MKCoordinateSpanMake(0.5, 0.5);
+        MKCoordinateSpan regionSpan = MKCoordinateSpanMake(0.2, 0.2);
         
         MKCoordinateRegion region = MKCoordinateRegionMake(userLocationCoordinate, regionSpan);
         self.mapView.region = region;
